512e83568c13c47a1bd5bc36ac17bcd466f4fad4
Sara Alkhamri <s.alkhamri@gmail.com>
2021-11-19 16:01:08 -0800

    initial commit

--- client/src/Login.js
+++ client/src/Login.js
@@ -8,7 +8,7 @@ const AUTH_URL =
 export default function Login() {
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-            <a className="btn btn-success btn-lg" href={AUTH_URL}>Login With Spotify</a>
+            <a className="btn btn-success btn-lg" href={AUTH_URL}>Login With Spotify!</a>
         </Container>
     )
 }
