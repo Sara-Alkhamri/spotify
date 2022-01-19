dcd71eea5a12af23306a18a1d59a0094576d70cf
Sara Alkhamri <s.alkhamri@gmail.com>
2022-01-18 20:37:08 -0800

    login updates

--- client/src/Login.js
+++ client/src/Login.js
@@ -4,7 +4,7 @@ import logo from './spotify-logo.png'
 import { Container } from 'react-bootstrap'
 
 const AUTH_URL =
-    "https://accounts.spotify.com/authorize?client_id=40e8b276f23c4303abf74945b97b12f1&response_type=code&redirect_uri=https://spotify-sara-alkhamri.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+    "https://accounts.spotify.com/authorize?client_id=40e8b276f23c4303abf74945b97b12f1&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
     return (
